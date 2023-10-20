2dba72859744015a1a2791c4e814abab98ffe62a
Pedro P. Alvarez Portelles <ppalvarez2002@gmail.com>
2023-10-20 19:41:17 -0400

    Added AspNetCore and fixed bug on [Contracts]

--- Contracts/Dtos/Gallery/PhotoUploadRequest.cs
+++ Contracts/Dtos/Gallery/PhotoUploadRequest.cs
@@ -1,7 +1,10 @@
+using Microsoft.AspNetCore.Http;
+
 namespace Contracts.Dtos.Gallery;
 
-public class PhotoUploadDto
+public class PhotoUploadRequest
 {
     public string Title { get; set; } = null!;
     public string Description { get; set; } = null!;
+    public IFormFile File { get; set; } = null!;
 }
